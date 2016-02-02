c317a12b50f22fa37df0be7a099d659bd90b2b2e
sebjak <sjak90@gmail.com>
2016-02-02 22:59:21 +0100

    Add spark to project, add hello world

--- src/main/java/com/blabla/controller/user/UserController.java
+++ src/main/java/com/blabla/controller/user/UserController.java
@@ -1,7 +1,14 @@
 package com.blabla.controller.user;
 
+import static spark.Spark.get;
+import static spark.Spark.post;
+
 /**
  * Created by Sebastian on 2016-02-02.
  */
 public class UserController {
+
+    public static void main(String[] args) {
+        get("/hello", (request, response) -> "Hello world User");
+    }
 }
--- src/main/java/com/blabla/service/api/user/UserService.java
+++ src/main/java/com/blabla/service/api/user/UserService.java
@@ -4,4 +4,5 @@ package com.blabla.service.api.user;
  * Created by Sebastian on 2016-02-02.
  */
 public interface UserService {
+
 }
